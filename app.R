03c0843f1612fe27278f23f1e1c1091c0877b4a0
mk <mkao006@gmail.com>
2018-04-12 17:02:20 +1200

    changed variable id

--- app.R
+++ app.R
@@ -9,8 +9,6 @@ shape_level = levels(model_var$shape)
 clarity_level = levels(model_var$clarity)
 cut_level = levels(model_var$cut)
 color_level = levels(model_var$color)
-polish_level = levels(model_var$polish)
-symmetry_level = levels(model_var$symmetry)
 
 ## Define UI
 ui <- fluidPage(
@@ -21,26 +19,26 @@ ui <- fluidPage(
 
         sidebarPanel(
             ## Shape
-            selectInput(inputId = "shapeSelection",
+            selectInput(inputId = "shape",
                         label = "Shape",
                         choices = shape_level,
                         selected = "round"),
             ## Carat
-            numericInput(inputId = "caratInput",
+            numericInput(inputId = "carat",
                          label = "Carat",
                          value = 0.5),
             ## Clarity
-            selectInput(inputId = "claritySelection",
+            selectInput(inputId = "clarity",
                         label = "Clarity",
                         choices = clarity_level),
 
             ## Cut
-            selectInput(inputId = "cutSelection",
+            selectInput(inputId = "cut",
                         label = "Cut",
                         choices = cut_level),
 
             ## Color
-            selectInput(inputId = "colorSelection",
+            selectInput(inputId = "color",
                         label = "Color",
                         choices = color_level)
         ),
@@ -60,11 +58,11 @@ ui <- fluidPage(
 server <- function(input, output){
 
     output$expectedPrice = renderText({
-        newdf = data.frame(shape = input$shapeSelection,
-                           carat = input$caratInput,
-                           clarity = input$claritySelection,
-                           cut = input$cutSelection,
-                           color = input$colorSelection)
+        newdf = data.frame(shape = input$shape,
+                           carat = input$carat,
+                           clarity = input$clarity,
+                           cut = input$cut,
+                           color = input$color)
         print(newdf)
         exp(predict(model, newdata = newdf))
     })
